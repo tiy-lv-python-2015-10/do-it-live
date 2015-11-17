b18ad4c5a46b3183ceee78e1d1eae02361bd2e9d
cesarmarroquin <cesarm2333@gmail.com>
2015-11-16 16:43:04 -0800

    removed from heroku logs

--- freds_list/heroku-settings.py
+++ freds_list/heroku-settings.py
@@ -28,40 +28,5 @@ STATICFILES_DIRS = (
 
 
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
-        }
-    },
-    'handlers': {
-        'file': {
-            'level': "DEBUG",
-            'class': 'logging.FileHandler',
-            'filename': os.path.join(BASE_DIR, "logs/general_logs.log"),
-            'formatter': 'verbose'
-        },
-        'mailer': {
-            'level': 'ERROR',
-            'class': 'django.utils.log.AdminEmailHandler'
-        },
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-            'formatter': 'verbose'
-        }
-
-    },
-    'loggers': {
-        'chirp': {
-            'handlers': ['file', 'console', 'mailer'],
-            'level': 'DEBUG',
-            'propagate': True
-        },
-    }
-
-}
 
 
